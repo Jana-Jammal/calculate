383c10f61dcc137fadc9208ce69247657348f931
jana <janajammal065@gmail.com>
2025-06-14 21:30:39 +0300

    first commit

---
+++ LEC2.Core/Calculater.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LEC2.Core;
+
+public class Calculater
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+}
+  public static int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Error");
+            return a / b;
+        }
+
+        public static bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+    }
+
---
+++ LEC2.Tests/Calculatertest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using LEC2.Core;
+namespace LEC2.Tests;
+
+public class Calculatertest
+{
+    [Fact]
+    public void Multiply_TwoPositiveNumber_ReturnCorrectProduct()
+    {
+        int a = 3;
+        int b = 2;
+        int result = Calculater.Multiply(a, b);
+        Assert.Equal(6, result);
+    }
+      [Fact]
+        public void Divide_ShouldReturnTrueResaul()
+        {
+            int a = 10;
+            int b = 2;
+            int result = Calculater.Divide(a, b);
+            Assert.Equal(5, result);
+        }
+      [Fact]
+        public void IsEven_ShouldReturnTrue_WhenNumberIsEven()
+        {
+            int number = 8;
+            bool result = Calculater.IsEven(number);
+            Assert.True(result);
+        }
+          [Fact]
+        public void Divide_ShouldThrowError_WhenDivideByZero()
+        {
+            int a = 10;
+            int b = 0;
+            Assert.Throws<DivideByZeroException>(() => Calculater.Divide(a, b));
+        }
+
+}
