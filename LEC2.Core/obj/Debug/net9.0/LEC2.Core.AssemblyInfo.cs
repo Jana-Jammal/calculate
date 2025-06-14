8928085bf5579abda590c6e8cbf42d4497b499a8
jana <janajammal065@gmail.com>
2025-06-14 23:16:36 +0300

    add more tests

--- LEC2.Core/obj/Debug/net9.0/LEC2.Core.AssemblyInfo.cs
+++ LEC2.Core/obj/Debug/net9.0/LEC2.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LEC2.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383c10f61dcc137fadc9208ce69247657348f931")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2107c0080e939a5ea582f860caa5c0bf03d782a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LEC2.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LEC2.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
